1b869927093c04472c68a48dbd35531140d8808f
Marxxie <121305567+Marxxie@users.noreply.github.com>
2022-12-23 12:35:36 -0500

    Update RobloxHopefully.lua
    reverse array (broken) test

--- RobloxHopefully.lua
+++ RobloxHopefully.lua
@@ -1,1 +1,14 @@
-print("hello")
+local function reverse(array)
+
+    for i = 1, #array do
+        array[i-#array] = array[i]
+    end
+
+    return array
+end
+
+local result = reverse({1, 2, 3})
+
+for i, v in pairs(result) do
+    print(i.."\n"..v)
+end
