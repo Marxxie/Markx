224385d97f4d1510d89defd8b7cc728a26cd049e
Marxxie <121305567+Marxxie@users.noreply.github.com>
2022-12-23 13:38:44 -0500

    Update RobloxHopefully.lua

--- RobloxHopefully.lua
+++ RobloxHopefully.lua
@@ -1,80 +1,81 @@
 local DSS = game:GetService("DataStoreService")
 local MyData = DSS:GetDataStore("MyData")
 
-local data = {}
 
 local Presets = {
-    {"Strength", 0},
-    {"Psychic", 0},
-    {"Endurance", 0}
+	{"Strength", 0},
+	{"Psychic", 0},
+	{"Endurance", 0}
 }
 
 
 local Values = {
-    {"NumberValue", nil},
-    {"NumberValue", nil},
-    {"NumberValue", nil}
+	{"NumberValue", nil},
+	{"NumberValue", nil},
+	{"NumberValue", nil}
 }
 
 
 local function CreateStats(player,  Matrices)
 
-    local Folder = Instance.new("Folder")
-    Folder.Name = "leaderstats"
-    Folder.Parent = player
-    
+	local Folder = Instance.new("Folder")
+	Folder.Name = "leaderstats"
+	Folder.Parent = player
 
-    for i, v in Matrices do
-        local Value = Instance.new(Matrices[1])
-        Value.Parent = Folder
-        Value.Value = Matrices[2]
-    end
+
+	for i, v in Matrices do
+		local Value = Instance.new(Matrices[1][1])
+		Value.Value = Matrices[i][2]
+		Value.Name = Presets[i][1]
+		Value.Parent = Folder
+	end
 
 end
 
 
 local function LoadData(player)
+	
+	local data
+	
+	local success, err = pcall(function()
+		data = MyData:GetAsync(tostring(player.UserId))
+	end)
+  
+	if not data[1] then
+		for i, v in Values do
+			v[2] = Presets[i][2]
+		end
+	else
+		for i, v in Values do
+			v[2] = data[i][2]
+		end
+	end
 
-   local success, err = pcall(function()
-       data = MyData:GetAsync(tostring(player.UserId))
-   end)
-
-
-   if not data then
-      for i, v in Values do
-        v[2] = Presets[i][2]
-      end
-    else
-        for i, v in Values do
-            v[2] = data[i][2]
-        end
-   end
-print(data, Values, Presets, err, success)
-   if not success then
-      warn(err)
-   end
-   
-   CreateStats(player, Values)
-
+	if not success then
+		warn(err)
+	end
+	
+	CreateStats(player, Values)
+    
 end
 
 local function SaveData(player)
 
-    data = {}
+	local data = {}
 
-    local folder = player.leaderstats
+	local folder = player.leaderstats
 
-    for i, v in folder:GetChildren() do
-        data[i] = {v.Name, v.Value}
-    end
+	for i, v in folder:GetChildren() do
+		data[i] = {v.Name, v.Value}
+	end
+	
+	local success, err = pcall(function()
+		MyData:SetAsync(tostring(player.UserId), data)
+	end)
 
-    local success, err = pcall(function()
-        MyData:SetAsync(tostring(player.UserId), data)
-    end)
-
-    if not success then
-        warn(err)
-    end
+	if not success then
+		warn(err)
+	end
 
 end
 
@@ -84,8 +85,8 @@ game.Players.PlayerRemoving:Connect(SaveData)
 
 game:BindToClose(function()
 
-    for _, Player in game.Players:GetPlayers() do
-        SaveData(Player)
-    end
+	for _, Player in game.Players:GetPlayers() do
+		SaveData(Player)
+	end
 
 end)
